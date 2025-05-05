2a3f4bb24cc88bbb4d52b10536b3f5abc3cb2e9c
Alex <voronko20172017@gmail.com>
2025-05-05 16:13:46 +0300

    update

---
+++ meta/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2224963.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [225144486, 225144491, 225144492, 225144496, 225144497, 225144511, 225144518, 225144529, 225144530, 225144532, 225144533, 225144534, 225662144, 225144535, 225144537, 225144539, 225144542, 225144543, 225144544, 225144545, 225144546, 225144556, 225616014, 225616015, 225616016, 225619871, 225624810, 225635716, 225669189, 225896610, 225915959, 225924487, 226036296, 226058324, 226172207, 226209596, 226272002];
+window.CONFIG_FILENAME = window.ASSET_PREFIX + "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    { 'moduleName': 'Ammo', 'glueUrl': 'files/assets/225144488/1/ammo.wasm.js', 'wasmUrl': 'files/assets/225144489/1/ammo.wasm.wasm', 'fallbackUrl': 'files/assets/225144487/1/ammo.js', 'preload': true },
+];
