9a1262165583380747efb865b3c9af35149c48ed
Alex <voronko20172017@gmail.com>
2025-06-03 11:43:45 +0300

    update

--- __settings__.js
+++ __settings__.js
@@ -1,6 +1,6 @@
 window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
-window.SCENE_PATH = "2243714.json";
+window.SCENE_PATH = "2253476.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
@@ -8,7 +8,7 @@ window.CONTEXT_OPTIONS = {
     'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "high-performance"
 };
-window.SCRIPTS = [ 230029519, 230032706, 230035148, 230254758, 230275435, 230296246, 230297730 ];
+window.SCRIPTS = [  ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,5 +18,4 @@ window.INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
-    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/230294034/1/draco.wasm.js', 'wasmUrl' : 'files/assets/230294035/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/230294033/1/draco.js', 'preload' : true},
 ];
